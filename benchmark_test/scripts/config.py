359d3357d225e69bdc3e621d88a786fe5d6fc8e2
Bennu-Li <53458891+Bennu-Li@users.noreply.github.com>
2020-04-16 17:40:23 +0800

    sift1m data link
    Former-commit-id: c464b3fe48e123ddccabeaa784db7f8306a70aa7 [formerly be58c0d61755b76d64dc5a0b6502b933722b5f30]
    Former-commit-id: 38879b0e77dc8254c6eefb73e5f99901d30c16d7

--- benchmark_test/scripts/config.py
+++ benchmark_test/scripts/config.py
@@ -2,7 +2,7 @@ from milvus import *
 import os
 
 MILVUS_HOST = "127.0.0.1"
-MILVUS_PORT = 19540
+MILVUS_PORT = 19530
 
 # create table param
 INDEX_FILE_SIZE = 2048
@@ -29,8 +29,8 @@ EFCONSTRUCTION = 500
 
 
 # insert param
-FILE_TYPE = 'bvecs'
-FILE_NPY_PATH = ''
+FILE_TYPE = 'npy'
+FILE_NPY_PATH = 'query_data'
 FILE_CSV_PATH = '/data1/lym/dataset_test/csv_dataset'
 FILE_FVECS_PATH = '/mnt/data/base.fvecs'
 FILE_BVECS_PATH = '/data1/workspace/milvus_data/sift_data/bigann_base.bvecs'
@@ -45,10 +45,10 @@ if_normaliz = False
 NQ_FOLDER_NAME = '/data1/workspace/milvus_data/sift_data/query_data'
 PERFORMANCE_FILE_NAME = 'performance'
 
-nq_scope = [1,10,100,200]
-topk_scope = [1,1, 10, 100,500]
-#nq_scope = [1, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]
-#topk_scope = [1,1, 20, 50, 100, 300, 500, 800, 1000]
+# nq_scope = [1,10,100,200]
+# topk_scope = [1,1, 10, 100,500]
+nq_scope = [1, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800]
+topk_scope = [1,1, 20, 50, 100, 300, 500, 800, 1000]
 IS_CSV = False
 IS_UINT8 = False
 
