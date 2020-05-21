50048c434b21f5674d720c55311f636182275235
shiyu22 <cshiyu22@gmail.com>
2020-05-21 11:32:07 +0800

    update
    Former-commit-id: f74d2da38693912ee43c77c662f5af4f1cbe8337 [formerly 2cb5148b7e809cb1b6b159a2fdee5f31b7562483]
    Former-commit-id: e81b6c4aceed7e91cfbc993b70de8eeb08f6cc32

--- solutions/recommender_system/infer_paddle.py
+++ solutions/recommender_system/infer_paddle.py
@@ -105,15 +105,7 @@ def normaliz_data(vec_list):
 def milvus_test(usr_features, mov_features, ids):
     _HOST = '127.0.0.1'
     _PORT = '19530'  # default value
-    milvus = Milvus()
-
-    param = {'host': _HOST, 'port': _PORT}
-    status = milvus.connect(**param)
-    if status.OK():
-        print("\nServer connected.")
-    else:
-        print("\nServer connect fail.")
-        sys.exit(1)
+    milvus = Milvus(_HOST, _PORT)
 
     table_name = 'paddle_demo1'
 
@@ -136,7 +128,7 @@ def milvus_test(usr_features, mov_features, ids):
     status, result = milvus.count_collection(table_name)
     print("rows in table paddle_demo1:", result)
 
-    status, table = milvus.count_collection(table_name)
+    status, table = milvus.count_entities(table_name)
 
     search_vectors = normaliz_data([mov_features.tolist()])
     param = {
