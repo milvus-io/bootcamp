e81b6c4aceed7e91cfbc993b70de8eeb08f6cc32
shiyu22 <cshiyu22@gmail.com>
2020-05-21 11:32:07 +0800

    update
    Former-commit-id: 2cb5148b7e809cb1b6b159a2fdee5f31b7562483

--- solutions/mols_search/mols-search-webserver/src/indexer/index.py
+++ solutions/mols_search/mols-search-webserver/src/indexer/index.py
@@ -6,8 +6,7 @@ import traceback
 
 def milvus_client():
     try:
-        milvus = Milvus()
-        status = milvus.connect(MILVUS_HOST, MILVUS_PORT, timeout=60)
+        milvus = Milvus(MILVUS_HOST, MILVUS_PORT)
         return milvus
     except Exception as e:
         log.error(e)
@@ -64,7 +63,6 @@ def has_table(client, table_name):
 
 
 def count_table(client, table_name):
-    status, num = client.count_collection(collection_name=table_name)
+    status, num = client.count_entities(collection_name=table_name)
     print(status)
-    # status, num = client.count_table(table_name=table_name)
     return num
