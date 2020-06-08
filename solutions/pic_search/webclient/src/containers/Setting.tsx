8ce168e2d5ddcc9a0ed0f413a503e73d79597c62
zhen.an <az15921869329@gmail.com>
2020-06-08 09:54:32 +0800

    done
    Former-commit-id: 8c6b5ac8d0bbb04ca62db3472edbd3021865c74f

--- solutions/pic_search/webclient/src/containers/Setting.tsx
+++ solutions/pic_search/webclient/src/containers/Setting.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { queryContext } from "../contexts/QueryContext";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import TextField from "@material-ui/core/TextField";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
@@ -12,144 +13,145 @@ import { baseColor } from "../utils/color";
 import Logo from "./Logo.svg";
 import { delayRunFunc } from "../utils/Helper";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    setting: {
-      display: "flex",
-      flexDirection: "column",
-      minWidth: "400px",
-      padding: "60px 20px",
-      borderWidth: "1px",
-      backgroundColor: "#1F2023",
-      color: "#E4E4E6",
-      overflowY: "auto"
-    },
-    header: {
-      marginBottom: "30px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center"
-    },
-    configHead: {
-      marginBottom: "30px",
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center"
-    },
-    config: {
-      fontSize: "24px",
-      color: "#FAFAFA"
-    },
-    clear: {
-      color: baseColor,
-      fontSize: "18px",
-      cursor: "pointer"
-    },
-    imageSet: {},
-    counts: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: "30px",
-      color: "#FAFAFA"
-    },
-    currTotal: {
-      fontSize: "12px"
-    },
-    setPath: {
-      display: "flex",
-      justifyContent: "start",
-      alignItems: "center",
-      marginBottom: "30px"
-    },
-    customInput: {
-      margin: "0 20px 0 0 !important",
-      color: "blue !important"
-    },
-    customFab: {
-      color: "#fff",
-      backgroundColor: baseColor,
-      width: "36px",
-      height: "36px",
-      "&:hover": {
-        backgroundColor: baseColor
-      }
-    },
-    customDeleteFab: {
-      position: "absolute",
-      top: "5px",
-      right: "5px",
-      color: "#fff",
-      backgroundColor: "#666769",
-      width: "24px",
-      height: "24px",
-      minHeight: "0px",
-      "&:hover": {
-        backgroundColor: "#666769"
-      }
-    },
-    customDelete: {
-      color: "#A7A7AF",
-      width: "18px",
-      height: "18px"
-    },
-    customIcon: {
-      color: "#fff",
-      backgroundColor: baseColor,
-      width: "20px",
-      height: "20px"
-    },
-    customSlider: {
-      color: baseColor,
-      marginBottom: "30px"
-    },
-    thumb: {
-      width: "16px",
-      height: "16px"
-    },
-    track: {
-      height: "4px",
-      borderRadius: "10px"
-    },
-    upload: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    benchImage: {
-      width: "400px",
-      height: "250px",
-      position: "relative"
-    },
-    dropzoneContainer: {
-      backgroundColor: "transparent",
-      width: "250px",
-      height: "250px",
-      borderRadius: "10px",
-      border: "solid .5px #C8C8C8",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    dropzoneText: {
-      fontSize: "14px",
-      color: "#B3B4B5",
-      marginBottom: "30px"
-    },
-    notchedOutline: {
-      borderWidth: ".5px",
-      borderColor: "#838385 !important"
-    },
-    formLabel: {
-      color: "#fff"
-    },
-    controlLabel: {
-      color: "#838385"
-    }
-  })
-);
-
 const Setting = (props: any) => {
+  const isMobile = !useMediaQuery("(min-width:1000px)");
+  const useStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+      setting: {
+        display: "flex",
+        flexDirection: "column",
+        minWidth: isMobile ? "90%" : "300px",
+        padding: "60px 20px",
+        borderWidth: "1px",
+        backgroundColor: "#1F2023",
+        color: "#E4E4E6",
+        overflowY: "auto",
+      },
+      header: {
+        marginBottom: "30px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      },
+      configHead: {
+        marginBottom: "30px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
+      config: {
+        fontSize: "24px",
+        color: "#FAFAFA",
+      },
+      clear: {
+        color: baseColor,
+        fontSize: "18px",
+        cursor: "pointer",
+      },
+      imageSet: {},
+      counts: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "30px",
+        color: "#FAFAFA",
+      },
+      currTotal: {
+        fontSize: "12px",
+      },
+      setPath: {
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+        marginBottom: "30px",
+      },
+      customInput: {
+        margin: "0 20px 0 0 !important",
+        color: "blue !important",
+        width: isMobile ? "80%" : "auto",
+      },
+      customFab: {
+        color: "#fff",
+        backgroundColor: baseColor,
+        width: "36px",
+        height: "36px",
+        "&:hover": {
+          backgroundColor: baseColor,
+        },
+      },
+      customDeleteFab: {
+        position: "absolute",
+        top: "5px",
+        right: "5px",
+        color: "#fff",
+        backgroundColor: "#666769",
+        width: "24px",
+        height: "24px",
+        minHeight: "0px",
+        "&:hover": {
+          backgroundColor: "#666769",
+        },
+      },
+      customDelete: {
+        color: "#A7A7AF",
+        width: "18px",
+        height: "18px",
+      },
+      customIcon: {
+        color: "#fff",
+        backgroundColor: baseColor,
+        width: "20px",
+        height: "20px",
+      },
+      customSlider: {
+        color: baseColor,
+        marginBottom: "30px",
+      },
+      thumb: {
+        width: "16px",
+        height: "16px",
+      },
+      track: {
+        height: "4px",
+        borderRadius: "10px",
+      },
+      upload: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      benchImage: {
+        width: "400px",
+        height: "250px",
+        position: "relative",
+      },
+      dropzoneContainer: {
+        backgroundColor: "transparent",
+        width: "250px",
+        height: "250px",
+        borderRadius: "10px",
+        border: "solid .5px #C8C8C8",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      dropzoneText: {
+        fontSize: "14px",
+        color: "#B3B4B5",
+        marginBottom: "30px",
+      },
+      notchedOutline: {
+        borderWidth: ".5px",
+        borderColor: "#838385 !important",
+      },
+      formLabel: {
+        color: "#fff",
+      },
+      controlLabel: {
+        color: "#838385",
+      },
+    });
+  });
   const { process, train, count, search, clearAll } = useContext(queryContext);
   const { setImages, loading, setLoading } = props;
   const classes = useStyles({});
@@ -169,7 +171,7 @@ const Setting = (props: any) => {
   const reader = new FileReader();
   reader.addEventListener(
     "load",
-    function() {
+    function () {
       if (benchImage.current) {
         benchImage.current.src = reader.result;
       }
@@ -291,27 +293,27 @@ const Setting = (props: any) => {
               shrink: true,
               classes: {
                 root: classes.controlLabel,
-                focused: classes.controlLabel
-              }
+                focused: classes.controlLabel,
+              },
             }}
             margin="normal"
             InputProps={{
               style: {
                 textAlign: "left",
-                width: "340px",
-                height: "40px"
+                width: isMobile ? "100%" : "340px",
+                height: "40px",
               },
               classes: {
                 notchedOutline: classes.notchedOutline,
-                root: classes.formLabel
+                root: classes.formLabel,
               },
-              placeholder: "path/to/your/images"
+              placeholder: "path/to/your/images",
             }}
           />
           <Fab
             classes={{
               root: classes.customFab,
-              focusVisible: classes.customFab
+              focusVisible: classes.customFab,
             }}
           >
             <AddIcon
@@ -333,7 +335,7 @@ const Setting = (props: any) => {
             root: classes.customSlider,
             track: classes.track,
             rail: classes.track,
-            thumb: classes.thumb
+            thumb: classes.thumb,
           }}
         />
       </div>
