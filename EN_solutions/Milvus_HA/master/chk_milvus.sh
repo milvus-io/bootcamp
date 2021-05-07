78955fd50c552d4706df8a287c3d8e7b548bb384
Mia <22544815+miia12@users.noreply.github.com>
2021-05-07 18:13:38 +0800

    Delete EN_solutions directory

--- EN_solutions/Milvus_HA/master/chk_milvus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RUNNING_STATUS=$(docker inspect --format '{{.State.Running}}' c5154606b258)
-
-
-if [[ "${RUNNING_STATUS}" != "true" ]];then
-    exit 1
-fi
-
--- EN_solutions/image_search2/webserver/src/service/theardpool.py
+++
@@ -1,8 +0,0 @@
-import threading
-from concurrent.futures import ThreadPoolExecutor
-from service.train import do_train
-
-
-def thread_runner(thread_num, func, *args):
-    executor = ThreadPoolExecutor(thread_num)
-    f = executor.submit(do_train, *args)
