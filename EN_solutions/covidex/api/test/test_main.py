33ceaa858640511f2849326a6f7f4386162c1c11
miia12 <qing.li@zilliz.com>
2021-05-08 11:19:44 +0800

    delete

--- EN_solutions/covidex/api/test/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-def test_status():
-    response = client.get("/api/status")
-    assert response.status_code == 200
--- solutions/pic_search/webserver/src/encoder/utils.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def get_imlist(path):
-    return [os.path.join(path, f) for f in os.listdir(path) if (f.endswith('.jpg') or f.endswith('.png'))]
