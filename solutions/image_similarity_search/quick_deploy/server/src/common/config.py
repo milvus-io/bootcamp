ff38231f7958be8f3cccbcba7166615f4d001bc0
Mia <qing.li@zilliz.com>
2021-05-21 14:22:21 +0800

    replace model to resnet50

--- solutions/image_similarity_search/quick_deploy/server/src/common/config.py
+++ solutions/image_similarity_search/quick_deploy/server/src/common/config.py
@@ -2,7 +2,8 @@ import os
 
 MILVUS_HOST = os.getenv("MILVUS_HOST", "127.0.0.1")
 MILVUS_PORT = os.getenv("MILVUS_PORT", 19530)
-VECTOR_DIMENSION = os.getenv("VECTOR_DIMENSION", 512)
-DATA_PATH = os.getenv("DATA_PATH", "/data/jpegimages")
-DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus")
-UPLOAD_PATH = "/tmp/search-images"
+VECTOR_DIMENSION = os.getenv("VECTOR_DIMENSION", 2048)
+DATA_PATH = os.getenv("DATA_PATH", "tmp/pictures")
+DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus_img_search")
+UPLOAD_PATH = "tmp/search-images"
+DEFAULT_CACHE_DIR = "./tmp"
---
+++ solutions/image_similarity_search/quick_deploy/server/src/preprocessor/resnet50.py
@@ -0,0 +1,25 @@
+import numpy as np
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input as preprocess_input_resnet50
+from keras.preprocessing import image
+from numpy import linalg as LA
+
+
+class Resnet50:
+    def __init__(self):
+        self.input_shape = (224, 224, 3)
+        self.weight = 'imagenet'
+        self.pooling = 'max'
+        self.model_resnet50 = ResNet50(weights='imagenet',
+                                       input_shape=(self.input_shape[0], self.input_shape[1], self.input_shape[2]),
+                                       pooling=self.pooling, include_top=False)
+        self.model_resnet50.predict(np.zeros((1, 224, 224, 3)))
+
+    def resnet50_extract_feat(self, img_path):
+        img = image.load_img(img_path, target_size=(self.input_shape[0], self.input_shape[1]))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input_resnet50(img)
+        feat = self.model_resnet50.predict(img)
+        norm_feat = feat[0] / LA.norm(feat[0])
+        return norm_feat.tolist()
