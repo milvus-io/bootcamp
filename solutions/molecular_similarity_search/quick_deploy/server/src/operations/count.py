31de9b4c9fb1babd16cf9cdc769ac92bc9d1c4be
Filip Haltmayer <filip.haltmayer@zilliz.com>
2021-06-14 16:54:29 -0700

    updated mol

---
+++ solutions/molecular_similarity_search/quick_deploy/server/src/operations/count.py
@@ -0,0 +1,14 @@
+import sys
+from src.logs import LOGGER
+from src.config import DEFAULT_TABLE
+
+
+def do_count(table_name, milvus_cli):
+    if not table_name:
+        table_name = DEFAULT_TABLE
+    try:
+        num = milvus_cli.count(table_name)
+        return num
+    except Exception as e:
+        LOGGER.error(" Error with count table {}".format(e))
+        sys.exit(1)
