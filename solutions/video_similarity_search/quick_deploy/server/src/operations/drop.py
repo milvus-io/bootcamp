27a36ba53be3483097ed8cf373db39e67569372a
shiyu22 <shiyu.chen@zilliz.com>
2021-06-16 18:40:23 +0800

    video_search

---
+++ solutions/video_similarity_search/quick_deploy/server/src/operations/drop.py
@@ -0,0 +1,17 @@
+import sys
+from logs import LOGGER
+
+sys.path.append("..")
+from config import DEFAULT_TABLE
+
+
+def do_drop(table_name, milvus_cli, mysql_cli):
+    if not table_name:
+        table_name = DEFAULT_TABLE
+    try:
+        status = milvus_cli.delete_collection(table_name)
+        mysql_cli.delete_table(table_name)
+        return status
+    except Exception as e:
+        LOGGER.error(" Error with  drop table: {}".format(e))
+        sys.exit(1)
