6f5afc0ddecf9b152b39c301f42feda9a9c71200
NotRyan <ryan.chan@zilliz.com>
2021-06-20 16:41:18 -0700

    Redid source code for the audio_similarity_search deployment

---
+++ solutions/audio_similarity_search/quick_deploy/server/src/operations/drop.py
@@ -0,0 +1,15 @@
+import sys
+from src.logs import LOGGER
+from src.config import DEFAULT_TABLE
+
+
+def do_drop(table_name, milvus_cli, mysql_cli):
+    if not table_name:
+        table_name = DEFAULT_TABLE
+    try:
+        status = milvus_cli.delete_collection(table_name)
+        mysql_cli.delete_table(table_name)
+        return status
+    except Exception as e:
+        LOGGER.error(" Error attempting to drop table: {}".format(e))
+        sys.exit(1)
