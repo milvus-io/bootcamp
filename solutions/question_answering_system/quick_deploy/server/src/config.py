a28befb5bd84ceeef72a9acd24a3f7f98802806e
Bennu <yunmei.li@zilliz.com>
2021-06-24 16:25:21 +0800

    QA 2.0

--- solutions/question_answering_system/quick_deploy/server/src/config.py
+++ solutions/question_answering_system/quick_deploy/server/src/config.py
@@ -1,17 +1,18 @@
 import os
-from milvus import MetricType
 
 ############### Milvus Configuration ###############
 MILVUS_HOST = os.getenv("MILVUS_HOST", "127.0.0.1")
+# MILVUS_HOST = os.getenv("MILVUS_HOST", "192.168.1.85")
 MILVUS_PORT = os.getenv("MILVUS_PORT", 19530)
 VECTOR_DIMENSION = os.getenv("VECTOR_DIMENSION", 768)
 INDEX_FILE_SIZE = os.getenv("INDEX_FILE_SIZE", 1024)
-METRIC_TYPE = os.getenv("METRIC_TYPE", MetricType.IP)
-DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus_qa_search")
+METRIC_TYPE = os.getenv("METRIC_TYPE", "IP")
+DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus_qa_search_1")
 TOP_K = os.getenv("TOP_K", 10)
 
 ############### MySQL Configuration ###############
 MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
+# MYSQL_HOST = os.getenv("MYSQL_HOST", "192.168.1.85")
 MYSQL_PORT = os.getenv("MYSQL_PORT", 3306)
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PWD = os.getenv("MYSQL_PWD", "123456")
