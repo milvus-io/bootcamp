9654cb3731fe8dea2ded7dbbddbb84030de13a96
Mia <qing.li@zilliz.com>
2021-06-24 18:02:36 +0800

    update code for 2.0

--- solutions/reverse_image_search/object_detection/server/src/config.py
+++ solutions/reverse_image_search/object_detection/server/src/config.py
@@ -1,12 +1,11 @@
 import os
-from milvus import MetricType
 
 ############### Milvus Configuration ###############
-MILVUS_HOST = os.getenv("MILVUS_HOST", "127.0.0.1")
-MILVUS_PORT = os.getenv("MILVUS_PORT", 19530)
+MILVUS_HOST = os.getenv("MILVUS_HOST", "192.168.1.85")
+MILVUS_PORT = os.getenv("MILVUS_PORT", 19533)
 VECTOR_DIMENSION = os.getenv("VECTOR_DIMENSION", 2048)
 INDEX_FILE_SIZE = os.getenv("INDEX_FILE_SIZE", 1024)
-METRIC_TYPE = os.getenv("METRIC_TYPE", MetricType.L2)
+METRIC_TYPE = os.getenv("METRIC_TYPE", "L2")
 DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus_obj_det")
 TOP_K = os.getenv("TOP_K", 10)
 
@@ -20,8 +19,10 @@ MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PWD = os.getenv("MYSQL_PWD", "123456")
 MYSQL_DB = os.getenv("MYSQL_DB", "mysql")
 
-DATA_PATH = os.getenv("DATA_PATH", "data")
-UPLOAD_PATH = "data/search-images"
+############### Model PATH Configuration ###############
 COCO_MODEL_PATH = os.getenv("OBJECT_PATH", "./yolov3_detector/data/yolov3_darknet")
 YOLO_CONFIG_PATH = os.getenv("OBJECT_PATH", "./yolov3_detector/data/yolov3_darknet/yolo.yml")
+
 CACHE_DIR = "./tmp"
+DATA_PATH = os.getenv("DATA_PATH", "data")
+UPLOAD_PATH = "data/search-images"
