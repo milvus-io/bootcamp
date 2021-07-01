b01497a856c545f510b40cacf86194824c9d6e0e
jingkl <34296482+jingkl@users.noreply.github.com>
2021-07-01 10:04:18 +0800

    Update main.py

--- solutions/text_search_engine/quick_deploy/Milvus-bert-server/src/main.py
+++ solutions/text_search_engine/quick_deploy/Milvus-bert-server/src/main.py
@@ -14,6 +14,8 @@ from operations.load import import_data
 from operations.search import search_in_milvus
 from operations.count import do_count
 from operations.drop import do_drop
+from pydantic import BaseModel
+from typing import Optional
 
 
 
@@ -27,6 +29,9 @@ app.add_middleware(
 MILVUS_CLI = MilvusHelper()
 MYSQL_CLI = MySQLHelper()
 
+class Item(BaseModel):
+    Table: Optional[str] = None
+    File:str
 
 @app.post('/text/count')
 async def count_text(table_name: str = None):
@@ -50,8 +55,8 @@ async def drop_tables(table_name: str = None):
     except Exception as e:
         LOGGER.error(e)
         return {'status': False, 'msg': e}, 400
-    
-    
+
+
 @app.post('/text/load')
 async def load_text(file: UploadFile = File(...), table_name: str = None):
     try:
@@ -78,9 +83,11 @@ async def load_text(file: UploadFile = File(...), table_name: str = None):
 @app.get('/text/search')
 async def do_search_api(table_name: str = None, query_sentence: str = None):
     try:
-        ids,title, text, distances = search_in_milvus(table_name, query_sentence, MILVUS_CLI, MYSQL_CLI)
-        res = dict(zip(title, text))
-        #res = sorted(res.items(), key=lambda item: item[0])
+        ids,title, text, distances = search_in_milvus(table_name,query_sentence, MILVUS_CLI, MYSQL_CLI)
+        res = []
+        for p, d in zip(title, text):
+            dicts = {'title': p, 'content':d}
+            res+=[dicts]
         LOGGER.info("Successfully searched similar text!")
         return res
     except Exception as e:
@@ -89,4 +96,4 @@ async def do_search_api(table_name: str = None, query_sentence: str = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host='192.168.1.85', port=5000)
+    uvicorn.run(app=app, host='0.0.0.0', port=5000)
