b826d60be4ef55c05d3faf512a99042a7aa59256
Bennu <yunmei.li@zilliz.com>
2021-07-02 11:57:58 +0800

    benchmark test

--- benchmark_test/scripts/config.py
+++ benchmark_test/scripts/config.py
@@ -1,13 +1,13 @@
-from milvus import *
+# from milvus import *
 import os
 
-MILVUS_HOST = "127.0.0.1"
-MILVUS_PORT = 19530
+MILVUS_HOST = "192.168.1.85"
+MILVUS_PORT = 19537
 
 ##################### Collection Parameters ########################################################
 
-INDEX_FILE_SIZE = 2048
-METRIC_TYPE = MetricType.L2
+METRIC_TYPE = 'L2'
+VECTOR_DIMENSION = 128
 
 ##################### Indexing Parameters ##########################################################
 
@@ -25,6 +25,9 @@ KNNG = 100
 HNSW_M = 16
 EFCONSTRUCTION = 500
 
+# index ANNOY parameters
+N_TREE = 8
+
 ##################### Insert Parameters ############################################################
 
 # File type used for base and query
@@ -36,7 +39,7 @@ FILE_TYPE = [
 ]
 
 # Point to directory of file data.
-BASE_FILE_PATH = '/data1/workspace/lym/milvus_test/sift_data/sift1m/data'
+BASE_FILE_PATH = 'sift1m/data'
 
 # Does the data need to be normalized before insertion
 IF_NORMALIZE = False
@@ -47,13 +50,14 @@ IMPORT_CHUNK_SIZE = 20000
 ##################### Performance Test Parameters ##################################################
 
 # Location of the query files
-QUERY_FILE_PATH = '/data1/workspace/lym/milvus_test/sift_data/query_data'
+QUERY_FILE_PATH = 'data/query.npy'
 
 # Path to put performance results to, based on current directory.
 PERFORMANCE_RESULTS_PATH = 'performance'
 
 # Scope of performance results. For each NQ_Scope, all the TOPK values will be tested
-NQ_SCOPE = [1, 10, 100, 500, 1000]
+# NQ_SCOPE = [1, 10, 100, 500, 1000]
+NQ_SCOPE = [1, 10]
 TOPK_SCOPE = [1, 1, 10, 100, 500]
 
 ##################### Recall Test Parameters #######################################################
@@ -68,14 +72,17 @@ RECALL_TOPK = 500
 RECALL_CALC_SCOPE = [1, 10, 100, 500]
 
 # Location of query file, if it is a csv, and if it is stored as UINT8
-RECALL_QUERY_FILE = '/data1/workspace/lym/milvus_test/sift_data/query_data/query.npy'
+RECALL_QUERY_FILE = 'data/query.npy'
 
 IS_CSV = False
 IS_UINT8 = False
 
 # Location of ground truth file
-GROUNDTRUTH_FILE = '/data1/workspace/lym/milvus_test/sift_data/sift1m/gnd/ground_truth_1M.txt'
+GROUNDTRUTH_FILE = 'data/sift1m/gnd/ground_truth_1M.txt'
 
 # Result locations
 RECALL_RES = 'recall_result'
 RECALL_RES_TOPK = 'recall_result/recall_compare_out'
+
+# the number of log files will be saved
+LOGS_NUM = 1
