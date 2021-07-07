13261f20326cb90961386535cc31a135a4194d69
NotRyan <ryan.chan@zilliz.com>
2021-07-07 01:44:07 -0700

    Add audio bootcamp dockerfile, update readme

--- solutions/audio_similarity_search/quick_deploy/server/main.py
+++ solutions/audio_similarity_search/quick_deploy/server/main.py
@@ -106,4 +106,4 @@ async def drop_tables(table_name: str = None):
         return {'status': False, 'msg': e}, 400
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='127.0.0.1', port=8002)
+    uvicorn.run(app=app, host='0.0.0.0', port=8002)
