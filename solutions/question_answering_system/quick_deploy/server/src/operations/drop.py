c5bfb7faa0600214b459b1c2e1aabc90b9f350c0
Bennu <yunmei.li@zilliz.com>
2021-07-08 10:32:47 +0800

    Update drop.py

--- solutions/question_answering_system/quick_deploy/server/src/operations/drop.py
+++ solutions/question_answering_system/quick_deploy/server/src/operations/drop.py
@@ -10,10 +10,10 @@ def do_drop(table_name, milvus_cli, mysql_cli):
         table_name = DEFAULT_TABLE
     try:
         if not milvus_cli.has_collection(table_name):
-            return ("Milvus doesn't have a collection named {}".format(table_name))
+            return "Milvus doesn't have a collection named {}".format(table_name)
         status = milvus_cli.delete_collection(table_name)
         mysql_cli.delete_table(table_name)
-        return status
+        return status.message
     except Exception as e:
         LOGGER.error(" Error with  drop table: {}".format(e))
         sys.exit(1)
