ce960b9a05f7d8db81515fac0de932897e5443a9
jingkl <34296482+jingkl@users.noreply.github.com>
2021-07-14 09:58:37 +0800

    Delete solutions/text_search_engine/quick_deploy/client/node_modules directory

--- solutions/text_search_engine/quick_deploy/client/node_modules/react-bootstrap-typeahead/es/utils/size.js
+++
@@ -1,6 +0,0 @@
-export function isSizeLarge(size) {
-  return size === 'large' || size === 'lg';
-}
-export function isSizeSmall(size) {
-  return size === 'small' || size === 'sm';
-}
--- solutions/text_search_engine/quick_deploy/client/node_modules/react-router-dom/es/MemoryRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import warnAboutDeprecatedESMImport from "./warnAboutDeprecatedESMImport.js";
-warnAboutDeprecatedESMImport("MemoryRouter");
-
-import { MemoryRouter } from "../esm/react-router-dom.js";
-export default MemoryRouter;
