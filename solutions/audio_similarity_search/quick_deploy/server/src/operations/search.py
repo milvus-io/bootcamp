f94476584f990a5f2f9a3717e7a290c0c38b21cf
zilliz_support <zilliz_support@sol01@example.org>
2021-07-14 16:17:31 +0800

    audio

--- solutions/audio_similarity_search/quick_deploy/server/src/operations/search.py
+++ solutions/audio_similarity_search/quick_deploy/server/src/operations/search.py
@@ -1,9 +1,10 @@
+from os import path
 import sys
 from src.logs import LOGGER
 from src.config import TOP_K, DEFAULT_TABLE
 from src.encode import get_audio_embedding
 
-def do_search(table_name, audio_path, model, milvus_client, mysql_cli):
+def do_search(host,table_name, audio_path, model, milvus_client, mysql_cli):
     try:
         if not table_name:
             table_name = DEFAULT_TABLE
@@ -12,6 +13,9 @@ def do_search(table_name, audio_path, model, milvus_client, mysql_cli):
         vids = [str(x.id) for x in vectors[0]]
         paths = mysql_cli.search_by_milvus_ids(vids, table_name)
         distances = [x.distance for x in vectors[0]]
+        for i in range(len(paths)):
+             tmp = "http://" + str(host) + "/data?audio_path=" + str(paths[i])
+             paths[i] = tmp
         return vids, paths, distances
     except Exception as e:
         LOGGER.error(" Error with search : {}".format(e))
