3f059b8965fe173e890eda531e696eceb3140c46
jingkl <34296482+jingkl@users.noreply.github.com>
2021-07-14 09:58:37 +0800

    Delete solutions/text_search_engine/quick_deploy/client/node_modules directory

--- solutions/text_search_engine/quick_deploy/client/node_modules/react-bootstrap-typeahead/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Change Log
-
-This project adheres to [semantic versioning](http://semver.org/). Each release is documented on the [releases](https://github.com/ericgio/react-bootstrap-typeahead/releases) page.
--- solutions/text_search_engine/quick_deploy/client/node_modules/react-router-dom/es/BrowserRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import warnAboutDeprecatedESMImport from "./warnAboutDeprecatedESMImport.js";
-warnAboutDeprecatedESMImport("BrowserRouter");
-
-import { BrowserRouter } from "../esm/react-router-dom.js";
-export default BrowserRouter;
