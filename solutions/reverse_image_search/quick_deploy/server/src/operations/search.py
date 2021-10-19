87eaf7ff53c49f6a762938281866530fc6dc56e5
rafaeldss <rafaeldss199916@gmail.com>
2021-10-19 02:51:29 -0300

    fix the TOPK bug of reverse_image_search/quick_deploy

--- solutions/reverse_image_search/quick_deploy/server/src/operations/search.py
+++ solutions/reverse_image_search/quick_deploy/server/src/operations/search.py
@@ -1,16 +1,16 @@
 import sys
 
 sys.path.append("..")
-from config import TOP_K, DEFAULT_TABLE
+from config import DEFAULT_TABLE
 from logs import LOGGER
 
 
-def do_search(table_name, img_path, model, milvus_client, mysql_cli):
+def do_search(table_name, img_path, top_k, model, milvus_client, mysql_cli):
     try:
         if not table_name:
             table_name = DEFAULT_TABLE
         feat = model.resnet50_extract_feat(img_path)
-        vectors = milvus_client.search_vectors(table_name, [feat], TOP_K)
+        vectors = milvus_client.search_vectors(table_name, [feat], top_k)
         vids = [str(x.id) for x in vectors[0]]
         paths = mysql_cli.search_by_milvus_ids(vids, table_name)
         distances = [x.distance for x in vectors[0]]
