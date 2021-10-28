f14a791340b418cf5e46eef4a36e8df156a143eb
zilliz <zilliz@zillizdeMacBook-Pro.local>
2021-10-28 16:34:55 +0800

    update dna quick_deploy
    Signed-off-by: zilliz <zilliz@zillizdeMacBook-Pro.local>

--- solutions/dna_sequence_classification/quick_deploy/server/src/operations/load.py
+++ solutions/dna_sequence_classification/quick_deploy/server/src/operations/load.py
@@ -1,26 +1,21 @@
 import sys
-import os
-import time
-import numpy as np
-#import traceback
 import pandas as pd
-import pickle
-from sklearn import preprocessing
 
 sys.path.append("..")
-from config import DEFAULT_TABLE,TOP_K, VECTOR_DIMENSION, KMER_K
-from utils import *
-from logs import LOGGER
+from config import DEFAULT_TABLE, KMER_K
+from utils import build_kmers,train_vec
 
-# Function to replace sequence column with kmers in df
-def seq_to_kmers(df):
-    df['kmers'] = df.apply(lambda x: build_kmers(x['sequence'], KMER_K), axis =1)
-    df = df.drop(['sequence'],axis=1)
 
-# Get lists of sequences in k-mers and labels in text from dataframe
-def get_vectors(df):
-    seq_to_kmers(df)
-    words = list(df['kmers']) # list of all sequences in kmers
+def seq_to_kmers(df_table):
+    # Function to replace sequence column with kmers in df_table
+    df_table['kmers'] = df_table.apply(lambda x: build_kmers(x['sequence'], KMER_K), axis =1)
+    df_table = df_table.drop(['sequence'],axis=1)
+
+
+def get_vectors(df_table):
+    # Get lists of sequences in k-mers and labels in text from dataframe
+    seq_to_kmers(df_table)
+    words = list(df_table['kmers']) # list of all sequences in kmers
     texts = []
     for i in range(len(words)):
         texts.append(' '.join(words[i]))
@@ -28,18 +23,19 @@ def get_vectors(df):
     return vectors
 
 def format_data(ids, classes):
+    # Get lists of sequences in k-mers and labels in text from dataframe
     data = []
     for i in range(len(ids)):
         value = (str(ids[i]), str(classes[i]))
         data.append(value)
     return data
 
-# Import vectors to Milvus and data to Mysql respectively
 def import_data(collection_name, file_dir, milvus_cli, mysql_cli):
+    # Import vectors to Milvus and data to Mysql respectively
     if not collection_name:
         collection_name = DEFAULT_TABLE
     df = pd.read_table(file_dir)
-    class_name = collection_name+'_class'
+    # class_name = collection_name+'_class'
     vectors = get_vectors(df)
     ids = milvus_cli.insert(collection_name, vectors)
     milvus_cli.create_index(collection_name)
@@ -47,13 +43,3 @@ def import_data(collection_name, file_dir, milvus_cli, mysql_cli):
     mysql_cli.load_data_to_mysql(collection_name, format_data(ids, df['class']))
     return len(ids)
 
-"""
-from milvus_helpers import MilvusHelper
-from mysql_helpers import MySQLHelper
-if __name__ == "__main__":
-    COLLECTION_NAME = "test"
-    file = "/Users/mengjiagu/Desktop/human.txt"
-    MILVUS_CLI = MilvusHelper()
-    MYSQL_CLI = MySQLHelper()
-    import_data(COLLECTION_NAME, file, MILVUS_CLI, MYSQL_CLI)
-"""
--- solutions/dna_sequence_classification/quick_deploy/server/src/operations/search.py
+++ solutions/dna_sequence_classification/quick_deploy/server/src/operations/search.py
@@ -1,14 +1,10 @@
-import os
-import time
 import sys
-import numpy as np
-import pickle
 import pandas as pd
 
 sys.path.append("..")
 from config import TOP_K, DEFAULT_TABLE, KMER_K, SEQ_CLASS_PATH
 from logs import LOGGER
-from utils import *
+from utils import build_kmers,encode_seq
 
 def search_in_milvus(table_name, query_sentence, milvus_cli, mysql_cli):
     if not table_name:
@@ -25,20 +21,20 @@ def search_in_milvus(table_name, query_sentence, milvus_cli, mysql_cli):
         ids, results_classes = mysql_cli.search_by_milvus_ids(vids, table_name)
         distances = [x.distance for x in results[0]]
         df_class = pd.read_table(SEQ_CLASS_PATH)
-        class_dict = dict()
+        class_dict = {}
         for i in range(len(df_class)):
             class_dict[df_class['class'][i]] = df_class['gene_family'][i]
         seq_genes = [class_dict[int(x)] for x in results_classes]
         return ids, results_classes, seq_genes, distances
     except Exception as e:
-        LOGGER.error(" Error with search : {}".format(e))
+        LOGGER.error(f" Error with search : {e}")
         sys.exit(1)
 
 from milvus_helpers import MilvusHelper
 from mysql_helpers import MySQLHelper
 if __name__ == "__main__":
     COLLECTION_NAME = "human_dna"
-    query_sentence="ACGTTA"
+    QUERY="ACGTTA"
     MILVUS_CLI = MilvusHelper()
     MYSQL_CLI = MySQLHelper()
-    search_in_milvus(COLLECTION_NAME, query_sentence, MILVUS_CLI, MYSQL_CLI)
+    search_in_milvus(COLLECTION_NAME, QUERY, MILVUS_CLI, MYSQL_CLI)
