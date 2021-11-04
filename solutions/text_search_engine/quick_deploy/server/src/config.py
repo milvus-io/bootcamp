2eec03b64f04614e0e742dce986ac8b00b7340c3
jingkl <34296482+jingkl@users.noreply.github.com>
2021-11-04 18:27:06 +0800

    Update config.py

--- solutions/text_search_engine/quick_deploy/server/src/config.py
+++ solutions/text_search_engine/quick_deploy/server/src/config.py
@@ -2,7 +2,7 @@ import os
 
 ############### Milvus Configuration ###############
 MILVUS_HOST = os.getenv("MILVUS_HOST", "127.0.0.1")
-MILVUS_PORT = int(os.getenv("MILVUS_PORT", "19536"))
+MILVUS_PORT = int(os.getenv("MILVUS_PORT", "19530"))
 VECTOR_DIMENSION = int(os.getenv("VECTOR_DIMENSION", "768"))
 INDEX_FILE_SIZE = int(os.getenv("INDEX_FILE_SIZE", "1024"))
 METRIC_TYPE = os.getenv("METRIC_TYPE", "IP")
