7f644f27c7ecd48ad2f7e5b819b22ebabe713285
gouzi <530971494@qq.com>
2022-03-25 10:42:25 +0800

    solve issue [#1042](https://github.com/milvus-io/bootcamp/issues/1042)

--- solutions/question_answering_system/quick_deploy/server/src/mysql_helpers.py
+++ solutions/question_answering_system/quick_deploy/server/src/mysql_helpers.py
@@ -72,7 +72,10 @@ class MySQLHelper():
             self.cursor.execute(sql)
             results = self.cursor.fetchall()
             LOGGER.debug("MYSQL search by question.")
-            return results[0][0]
+            if results:
+                return results[0][0]
+            else:
+                return results
         except Exception as e:
             LOGGER.error(f"MYSQL ERROR: {e} with sql: {sql}".format(e, sql))
             sys.exit(1)
